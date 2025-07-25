0836187f3ade6918b57bd0cca552bc5ac83bd1fe
Vitor Eduardo Henkels <vitor.henkels@gmail.com>
2025-07-25 11:51:07 -0300

    animação

---
+++ lib/animations/capacity.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class OpacityExample extends StatefulWidget {
+  const OpacityExample({super.key});
+
+  @override
+  State<OpacityExample> createState() => _OpacityExampleState();
+}
+
+class _OpacityExampleState extends State<OpacityExample> {
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedOpacity(opacity: _visible? 1.0 : 00,
+              duration: ,)
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
